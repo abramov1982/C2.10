e81feb186f04fea79d57b9562bdd909dd49b8b3f
abramov1982 <49936583+abramov1982@users.noreply.github.com>
2020-02-09 16:50:38 +0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,21 +1,49 @@
-const progress = document.querySelector('.progress-bar')
+const vote = document.querySelector('.vote')
+const dog = document.querySelector('.dog')
+const parrot = document.querySelector('.parrot')
+const cat = document.querySelector('.cat')
+const result = document.querySelector('result')
 
-console.log('%O', progress)
+const url_cats = new URL('https://sf-pyw.mosyag.in/sse/vote/cats')
+const url_dogs = new URL('https://sf-pyw.mosyag.in/sse/vote/dogs')
+const url_parrots = new URL('https://sf-pyw.mosyag.in/sse/vote/parrots')
+const url_result = new URL('https://sf-pyw.mosyag.in/sse/vote/stats')
 
-const header = new Headers({
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Origin': '*'
-})
+const ES = new EventSource(url_result);
 
-const url = new URL('https://sf-pyw.mosyag.in/sse/stream-randoms')
-
-const ES = new EventSource(url, header)
-
-ES.onerror = error => {
-    ES.readyState ? progress.textContent = "Some error" : null;
+ES.onmessage = function (message) {
+    response = JSON.parse(message.data);
+    ES.close();
+    console.log(response)
+    let {cats, parrots, dogs} = response;
+    console.log(cats)
+    console.log(parrots)
+    console.log(dogs)
+    
 }
 
-ES.onmessage = ({ data }) => {
-    progress.style.cssText = `width: ${data}%`
-    progress.textContent = `${data}%`
-}
+
+function post_vote(url){
+    fetch(url, {
+        method: 'POST',
+        body: ''
+        })
+};
+
+function vote_display(){
+    vote.style.display = 'none'
+};
+dog.onclick = () => {
+    post_vote(url_dogs)
+    vote_display()
+};
+
+parrot.onclick = () => {
+    post_vote(url_parrots)
+    vote_display()
+};
+
+cat.onclick = () => {
+    post_vote(url_cats)
+    vote_display()
+};
